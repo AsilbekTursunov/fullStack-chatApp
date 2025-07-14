40d0f75ebf29b71e8656e00f5d56e2e9cbe2b9a9
asilbek_tursun <148640148+AsilbekTursunov@users.noreply.github.com>
2025-07-14 15:04:23 +0500

    change api/index files

--- lib/api/index.ts
+++ lib/api/index.ts
@@ -1,4 +1,3 @@
-import { useUserContext } from '@/hooks/useUserContext'
 import axios from 'axios'
 
 export const signUpUser = async (user: { fullName: string; email: string; password: string }) => {
@@ -8,49 +7,5 @@ export const signUpUser = async (user: { fullName: string; email: string; passwo
 
 export const loginUser = async (user: { email: string; password: string }) => {
 	const response = await axios.post('/api/auth/login', user)
-	return response.data
-}
-
-export const logoutUser = async () => {
-	const response = await axios.post('/api/auth/logout')
-	return response.data
-}
-
-export const getMe = async () => {
-	const response = await axios.get('/api/auth/me')
-	return response.data
-}
-
-export const sendFriendRequest = async (recipenter: string) => {
-	const { user } = useUserContext()
-	const response = await axios.post(`/api/users/friend-request`, {
-		recipientId: recipenter,
-		currentUserId: user?.id,
-	})
-	return response.data
-}
-
-export async function getOngoingFriendReqs() {
-	const { user } = useUserContext()
-	const response = await axios.get(`/api/users/ongoing-friend-request/${user?.id}`)
-	return response.data
-}
-
-export const getFriendRequests = async () => {
-	const { user } = useUserContext()
-	const response = await axios.get('/api/users/friends-requests', {
-		params: {
-			userId: user?.id,
-		},
-	})
-	return response.data
-}
-
-export const acceptFriendRequest = async (requestId: string) => {
-	const { user } = useUserContext()
-	const response = await axios.put(`/api/users/friend-request-accept`, {
-		requestId,
-		userId: user?.id,
-	})
 	return response.data
 }
